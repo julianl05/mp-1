20de657936933b4210f1a503edae9ff1d7980694
Julian Lim Jun Ren <limjunre@bu.edu>
2025-02-06 20:41:45 -0500

    added conditional red on negative output to calc

--- projects/script.js
+++ projects/script.js
@@ -2,28 +2,52 @@ function addition() {
     let first = Number(document.getElementById('first-num').value);
     let second = Number(document.getElementById('second-num').value);
     let result = first + second;
+
     document.getElementById('output').innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById('output').style.color = 'red';
+    } else {
+        document.getElementById('output').style.color = '#EEEEEE';
+    }
 }
 
 function subtraction() {
     let first = Number(document.getElementById('first-num').value);
     let second = Number(document.getElementById('second-num').value);
     let result = first - second;
+
     document.getElementById('output').innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById('output').style.color = 'red';
+    } else {
+        document.getElementById('output').style.color = '#EEEEEE';
+    }
 }
 
 function multiplication() {
     let first = Number(document.getElementById('first-num').value);
     let second = Number(document.getElementById('second-num').value);
     let result = first * second;
+
     document.getElementById('output').innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById('output').style.color = 'red';
+    } else {
+        document.getElementById('output').style.color = '#EEEEEE';
+    }
 }
 
 function division() {
     let first = Number(document.getElementById('first-num').value);
     let second = Number(document.getElementById('second-num').value);
     let result = first / second;
+
     document.getElementById('output').innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById('output').style.color = 'red';
+    } else {
+        document.getElementById('output').style.color = '#EEEEEE';
+    }
 }
 
 function power() {
@@ -36,10 +60,15 @@ function power() {
     }
 
     document.getElementById('output').innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById('output').style.color = 'red';
+    } else {
+        document.getElementById('output').style.color = '#EEEEEE';
+    }
 }
 
 function reset() {
-    console.log("testing");
+    // console.log("testing");
     document.getElementById('first-num').value = '';
     document.getElementById('second-num').value = '';
     document.getElementById('output').innerHTML = '';
